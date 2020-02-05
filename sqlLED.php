61040afb4fae93af2010eff15a38e3f6bc6b707d
isaseg <42422178+isaseg@users.noreply.github.com>
2020-02-05 08:33:49 -1000

    Update sqlLED.php

--- sqlLED.php
+++ sqlLED.php
@@ -1,35 +1,69 @@
-<?php
-$catch = $_REQUEST["turnOnOff"];
-$servername = "localhost";
-$username = "piControl";
-$password = "f103";
-$dbname = "piLight";
+<html>
+	<head>
+		<script>
+			<?php
+				//server and database info
+				$catch = $_REQUEST["turnOnOff"];
+				$servername = "localhost";
+				$username = "piControl";
+				$password = "f103";
+				$dbname = "piLight";
+				
+				//creating the connection
+				$conn = new mysqli($servername, $username, $password, $dbname);
+				if ($conn->connect_error) {
+					die("Connection failed: ".$conn->connect_error);
+				}
+				//if statea is 0, set switchLight to 0
+				if ($catch == 0) {
+					$sql = "UPDATE lightState SET switchLight=0";
+				}
+				//if statea is 1, set switchLight to 1
+				elseif ($catch == 1) {
+					$sql = "UPDATE lightState SET switchLight=1";
+				}
+				else {
+					$sql = "";
+				}
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-	die("Connection failed: ".$conn->connect_error);
-}
+				$result = $conn->query($sql);
+				$conn->close();
+				?>
+			<?php
+				$servername = "localhost";
+				$username = "piControl";
+				$password = "f103";
+				$dbname = "piLight";
 
-//$sql = "SELECT switchLight FROM lightState";
-//if ($result = $conn->query($sql)) {
-//	while ($row = $result -> fetch_row()) {
-//		printf ($row[0]);
-//	}
-//		$result->free_result();
-//}
-
-//if ($row[0] == 1) {
-if ($catch == 0) {
-	$sql = "UPDATE lightState SET switchLight=0";
-}
-//elseif ($row[0] == 0) {
-elseif ($catch == 1) {
-	$sql = "UPDATE lightState SET switchLight=1";
-}
-else {
-	$sql = "";
-}
-
-$result = $conn->query($sql);
-$conn->close();
-?>
+				$conn = new mysqli($servername, $username, $password, $dbname);
+				if ($conn->connect_error) {
+					die("Connection failed: ".$conn->connect_error);
+				}	
+				//get the switchLight value, set it to a var, statea
+				$sql = "SELECT switchLight FROM lightState";
+				if ($result = $conn->query($sql)) {
+					while ($row = $result -> fetch_row()) {
+						echo "var statea = ".$row[0];
+					}
+					$result->free_result();
+				}
+				$conn->close();
+			?>	
+			//refreshes the var
+			function switchLED() {
+				var xmlhttp = new XMLHttpRequest();
+				if (statea == 0) {
+					statea = 1;
+				}
+				else if (statea == 1) {
+					statea = 0;
+				}
+				xmlhttp.open("GET", "buttonLED.php?turnOnOff=" + statea, true);
+				xmlhttp.send();
+			}
+		</script>
+	</head>
+	<body>
+		<button onclick="switchLED()">Turn LED On/Off</button>
+	</body>
+</html>
