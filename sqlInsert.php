a48d3f377572229936b6ede30f041c74d6eab962
isaseg <42422178+isaseg@users.noreply.github.com>
2020-02-07 08:11:48 -1000

    Update sqlInsert.php

--- sqlInsert.php
+++ sqlInsert.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "api";
-$password = "f103";
-$dbname = "people";
+$username = "User";
+$password = "password";
+$dbname = "school";
 $name = 'Aaron';
 $age = 17;
 $gradeLevel = 11;
