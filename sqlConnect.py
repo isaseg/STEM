53f01791725f7085b56ee32979ece3317eaed2db
isaseg <42422178+isaseg@users.noreply.github.com>
2020-02-07 09:17:19 -1000

    Update sqlConnect.py

--- sqlConnect.py
+++ sqlConnect.py
@@ -2,16 +2,19 @@
 
 import MySQLdb
 
+#create the connection
 db = MySQLdb.connect(host="localhost", user="User", passwd="password", db="school")
-#create cursor
 
+#create cursor
 cur = db.cursor(MySQLdb.cursors.DictCursor)
 
 #Create table as per requirement
 sql = "SELECT * from students"
 
+#execute the cursor
 cur.execute(sql)
 
+#print the rows
 rows = cur.fetchall()
 for row in rows:
 	print(row)
