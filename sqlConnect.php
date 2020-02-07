1c03a52da80acb3ad16ec500b5da9b4f807c2aeb
isaseg <42422178+isaseg@users.noreply.github.com>
2020-02-07 08:09:59 -1000

    Update sqlConnect.php

--- sqlConnect.php
+++ sqlConnect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "api";
-$password = "f103";
-$dbname = "people";
+$username = "User";
+$password = "password";
+$dbname = "school";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
