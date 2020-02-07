a48560e96b0f6413a7ae9312ae8727f4a7879422
isaseg <42422178+isaseg@users.noreply.github.com>
2020-02-07 08:10:50 -1000

    Update sqlConnect.py

--- sqlConnect.py
+++ sqlConnect.py
@@ -2,7 +2,7 @@
 
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", user="api", passwd="f103", db="people")
+db = MySQLdb.connect(host="localhost", user="User", passwd="password", db="school")
 #create cursor
 
 cur = db.cursor(MySQLdb.cursors.DictCursor)
